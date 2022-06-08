3a0c66fd380cddaa9d6638d352639b2bca755b1c
izilongwan <js.nb@qq.com>
2022-06-08 18:35:16 +0800

    fix: 修改components导出方式

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
-import {
-  Button,
-  Confirm,
-  Icon,
-  Loading,
-  Notify,
-  VirtualList
-} from './components'
+import * as components from './components'
 import './styles/index.scss'
 
 export * from './components'
 export * from './hooks'
 
-export default {
-  Button,
-  Confirm,
-  Icon,
-  Loading,
-  Notify,
-  VirtualList,
-}
+export default components
